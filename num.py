d99c113338c0bd664d3c7119fc4f3b2144ca2bb9
Bharath Kumar Reddy Mopuru <95495923+BharathMopuru@users.noreply.github.com>
2023-09-27 08:42:43 -0500

    Update num.py

--- num.py
+++ num.py
@@ -2,7 +2,8 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from datetime import datetime
-specific_vehicle_number = 'XYZ45uhk6h'
+import main
+specific_vehicle_number = main.cleanString
 uri = "mongodb+srv://dheerajkittu27:12345@cluster0.izcphcw.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['vehicle_log']
@@ -18,9 +19,9 @@ vehicle_data = [
 collection.insert_many(vehicle_data)
 
 query = {'vehicle_number': specific_vehicle_number}
-documents = collectionE.find(query)
+documents = collectionE.count_documents(query)
 print(documents)
-if(documents.__sizeof__()!=0):
+if(documents==0):
     print("Not an emergency vehicle")
 else:
     print("activate emergency protocol")
