9d5c21014ff19ce7dbd2c41e0e75416c597dd661
kurrakovidh <111768926+kurrakovidh@users.noreply.github.com>
2023-11-08 08:41:10 -0600

    Revert "Merge branch 'main' of https://github.com/donthireddylokesh/GDP"
    This reverts commit e21ebd574e47c0620de9f6a75d63e5a25cc6a6e9, reversing
    changes made to de6f3dc20bd9a4f85a67ae5d24426ffdb9f0585b.

--- num.py
+++ num.py
@@ -2,33 +2,26 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from datetime import datetime
-
+specific_vehicle_number = 'XYZ45uhk6h'
 uri = "mongodb+srv://dheerajkittu27:12345@cluster0.izcphcw.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
-class dataBase:
-    def regTag(self,location):
-        specific_vehicle_number = self
-        dbE = client['Emergency_vehicle']
-        collectionE = dbE['Emergency_Vehicle']
-        query = {'vehicle_number': specific_vehicle_number}
-        documents = collectionE.count_documents(query)
-        print(documents)
-        if(documents==0):
-            print("Not an emergency vehicle")
-            return False
-        else:
-            print("activate emergency protocol")
-            dataBase.insertData(self,location)
-            return True
-        #for document in documents:
-          #  print(document + "none")
+db = client['vehicle_log']
+collection = db['junction1']
+now = datetime.now()
+dbE = client['Emergency_vehicle']
+collectionE = db['Emergency_Vehicle']
 
+# current_time = now.strftime("%H:%M:%S")
+vehicle_data = [
+    {'vehicle_number': specific_vehicle_number, 'time':now}
+]
+collection.insert_many(vehicle_data)
 
-    def insertData(self,location):
-        specific_vehicle_number = self
-        db = client['vehicle_log']
-        collection = db['junction1']
-        now = datetime.now()
-        # current_time = now.strftime("%H:%M:%S")
-        vehicle_data = [{'vehicle_number': specific_vehicle_number, 'time': now, 'Location': location}]
-        collection.insert_many(vehicle_data)
+query = {'vehicle_number': specific_vehicle_number}
+documents = collectionE.find(query)
+if(documents.__sizeof__()!=0):
+    print("Not an emergency vehicle")
+else:
+    print("activate emergency protocol")
+
+client.close()
